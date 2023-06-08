2dda6fe9dd2a31a55f8bf7e926e2fddd692db015
Christian Kallas <bkallas3@gmail.com>
2023-06-08 13:12:18 -0700

    completed to do list app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,65 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Is So Cool!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React is so cool!</p>
-        <p>So cool, is React!</p>
-        <ul>
-          <li>React</li>
-          <li>Is</li>
-          <li>So Cool</li>
-        </ul>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p> </p>
-      </header>
-    </div>
-  );
+import React from 'react'
+class App extends React.Component {
+  constructor (){
+    super();
+    this.state = {
+      list: ["ready", "set", "GO"],
+      text: "",
+    }
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+    onSubmit(event) {
+      event.preventDefault()
+      let newList = [...this.state.list, this.state.text]
+      this.setState({list: newList, text: " "})
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <form onSubmit = {this.onSubmit}>
+          <input type="text" value = {this.state.text} 
+          onChange={(e) => this.setState({text: e.target.value})}
+          />
+          <button type='submit'>Add</button>
+            <ul>
+            {this.state.list.map((task, id)=> {
+            return <li key={id}>{task}</li>
+            })}
+            </ul>
+        </form>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App"> 
+//       <header className="App-header">
+//         <h1>React Is So Cool!</h1>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>React is so cool!</p>
+//         <p>So cool, is React!</p>
+//         <ul>
+//           <li>React</li>
+//           <li>Is</li>
+//           <li>So Cool</li>
+//         </ul>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <p> </p>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
